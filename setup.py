67721d142d9a24c3dbaed5ced8bf8fa68bd6fa04
SNG Viraj Reddy <vir200319@gmail.com>
2023-09-27 20:27:33 +0530

    Update setup.py - OS classifier

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
